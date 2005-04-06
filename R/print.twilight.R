209bce956886e276d40ac9a7af0b4d8c088f50c3
Stefanie Scheid <stefanie.scheid@molgen.mpg.de>
2005-04-06 13:09:48 +0000

    including print function
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/twilight@11726 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/print.twilight.R
@@ -0,0 +1,32 @@
+print.twilight <- function(x, ...){
+
+  cat("\n Twilight object with")
+  cat("\n    ",nrow(x$result),"transcripts")
+  cat("\n     observed and expected test statistics")
+  cat("\n     p- and q-values")
+
+  if (!is.nan(x$result$fdr[1])){
+    cat("\n     local FDR")    
+  }
+  if (!is.nan(x$result$mean.fdr[1])){
+    cat("\n     bootstrap estimates of local FDR")    
+  }
+
+  cat("\n")
+  if (is.nan(x$boot.pi0[1])){
+    cat("\n Estimated percentage of non-induced genes:\n")
+    a <- x$pi0
+    names(a) <- "pi0"
+    print(a)
+  }
+  if (!is.nan(x$boot.pi0[1])){
+    cat("\n Bootstrap estimate of percentage of non-induced")
+    cat("\n genes with lower and upper ",x$boot.ci*100,"% CI:\n",sep="")
+    print(x$boot.pi0)
+  }
+      
+  cat("\n Function call:\n",x$call,"\n")
+  if (!is.nan(x$lambda)){
+    cat(" Function twilight used lambda =",x$lambda,"\n")
+  }
+}
