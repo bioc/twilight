96eef0cfe0ea6fb65e8482afff807e7fa5fd4e1a
Stefanie Scheid <stefanie.scheid@molgen.mpg.de>
2007-08-19 09:44:06 +0000

    fixed library.dynam
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/twilight@26498 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/a.R
+++ R/a.R
@@ -1,5 +1,5 @@
 .First.lib <- function(libname,pkgname,where){
-  library.dynam(pkgname,pkgname,libname)
+  library.dynam("twilight",pkgname,libname)
   
   if(.Platform$OS.type=="windows" && require(Biobase) && interactive() && .Platform$GUI=="Rgui"){
     addVigs2WinMenu("twilight")
