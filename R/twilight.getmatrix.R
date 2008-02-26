20cdf06b6ec309b660e010fcf994a4056dc3b7b3
Patrick Aboyoun <unknown>
2008-02-26 18:48:35 +0000

    Removed support for soon to be defunct class exprSet.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/twilight@30199 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/twilight.getmatrix.R
+++ R/twilight.getmatrix.R
@@ -1,17 +1,13 @@
 twilight.getmatrix <- function(xin){
-### Extracts the data matrix from an expression set.
-
-  if (class(xin)[1]=="exprSet"){
-    xout <- exprs(xin)
-    rownames(xout) <- geneNames(xin)
-    colnames(xout) <- sampleNames(xin)
-    return(xout)
-  } 
+  ## Extracts the data matrix from an expression set.
   if (class(xin)[1]=="ExpressionSet"){
     xout <- exprs(xin)
     rownames(xout) <- featureNames(xin)
     colnames(xout) <- sampleNames(xin)
-    return(xout)
+    result <- xout
   } 
-  if ((class(xin)[1]!="ExpressionSet")&(class(xin)[1]!="exprSet")){return(xin)}
+  else {
+	result <- xin
+  }
+  return(result)
 }
